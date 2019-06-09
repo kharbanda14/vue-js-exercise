5f72013a2c5f5aadd2d240a83caf0ad4c6589d41
Aman Kharbanda <amankharbanda34@gmail.com>
2019-06-09 10:09:04 +0530

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/**
+ * Importing Bootstrap
+ */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 Vue.config.productionTip = false
 
 new Vue({
